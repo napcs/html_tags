c2228694426237354a4b011a7a4d4f01d4c3a566
Brian Hogan <brianhogan@napcs.com>
2009-05-28 07:55:26 -0500

    Tests with database-backed form fields now - works much better. Time for a release

--- test/html_tags_test.rb
+++ test/html_tags_test.rb
@@ -4,7 +4,9 @@ class HtmlTagsTest < Test::Unit::TestCase
   
   include ActionView::Helpers::TagHelper
   include ActionView::Helpers::AssetTagHelper
-    ActionView::Helpers::AssetTagHelper.cache_asset_timestamps = false
+  include ActionView::Helpers::FormHelper
+  include ActionView::Helpers::FormTagHelper
+  
   
   def test_stylesheet_tag
     value = '<link href="/stylesheets/style.css" media="screen" rel="stylesheet" type="text/css">'
@@ -12,9 +14,28 @@ class HtmlTagsTest < Test::Unit::TestCase
   end
   
   def test_image_tag
-    value ='<img alt="Rails" src="/images/rails.png">'
-    assert_equal value, image_tag("rails.png")
+    value ='<img alt="Foo" src="/images/foo.png">'
+    assert_equal value, image_tag("foo.png")
     
   end
+  def test_form_tag
+    value = '<input id="foo" name="foo" type="text">'
+    assert_equal value, text_field_tag("foo")    
+  end
   
+  def test_should_have_database_working
+    t= Todo.new(:name => "foo")
+    assert t.save
+    Todo.delete_all
+  end
+  
+  def test_should_render_text_field
+    @todo = Todo.new
+    value = '<input id="todo_name" name="todo[name]" size="30" type="text">'
+    assert_equal value, text_field(:todo, :name)
+  end
+  
+    
+    
+    
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,4 +4,35 @@ require 'test/unit'
 # rails environment - load my plugin baby!
 require File.expand_path(File.join(File.dirname(__FILE__), '../../../../config/environment.rb'))
 require 'rubygems'
+require 'fileutils'
+
+require 'activerecord'
+DB = 'test.sqlite3.db'
+
+#FileUtils.rm DB
+
+
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3',
+  :dbfile =>  DB
+)
+
+class CreateDatabase < ActiveRecord::Migration
+  def self.up
+    create_table :todos, :force => true do |t|
+      t.string :name
+      t.boolean :complete
+      t.text :description
+      t.integer :priority
+    end
+  end
+end
+
+class Todo < ActiveRecord::Base
+  validates_presence_of :name
+end
+
+CreateDatabase.up unless File.exist?(DB)
+  
+  
 
